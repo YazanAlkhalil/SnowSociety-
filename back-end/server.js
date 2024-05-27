25cfc087f8d17e1e3d4b36a620186797850940bd
Malkalteer <alteermalk@gmail.com>
2024-05-27 16:08:58 +0300

    v23

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import mongoose from 'mongoose';
+import userRouter from './routes/users.js'
+import postRouter from './routes/posts.js'
 const app = express()
+
 const port=333;
 import router from './routes/authRoute.js'
 mongoose.connect("mongodb://127.0.0.1:27017/social").then(()=>{
@@ -9,6 +12,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/social").then(()=>{
 
 
 
-app.use('auth',router)
+app.use('/auth',router)
+app.use('/users',userRouter)
+app.use('/posts',postRouter)
 
 app.listen(port ,()=>console.log('listening on port'))
