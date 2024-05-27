7ec6ac93c4ed51cd302aa169bc5780e6e454f5a8
Ahmad <ahmadzer607@gmail.com>
2024-05-27 10:52:00 +0300

    create route regester

--- back-end/server.js
+++ back-end/server.js
@@ -1,1 +1,8 @@
 import express from 'express'
+import mongoose from 'mongoose';
+const app = express()
+const port=333;
+
+// mongoose.connect()
+
+app.listen(port ,()=>console.log('listening on port'))
