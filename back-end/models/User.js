3d2db1e66f7b2453471d88992b55a258ad9d07e1
Ahmad <ahmadzer607@gmail.com>
2024-05-27 10:52:52 +0300

    add route rigester

---
+++ back-end/models/User.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+const userSchema=new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        min:2,
+        max:50
+
+    },  
+    lastName:{
+        type:String,
+        required:true,
+        min:2,
+        max:50,
+
+    },
+      email:{
+        type:String,
+        required:true,
+        min:2,
+        max:50,
+        unique:true
+
+    },
+    passwords:{
+        type:String,
+        required:true,
+        min:2,
+        max:50,
+        unique:true
+    },
+    picturepath:{
+        type:String,
+        default:""
+    },
+    friends:{
+        type:Array,default:[]
+    },
+    location:String,
+    occupation:String,
+    viewedprofile:Number,
+    impression:Number
+},
+{
+    timestamps:true
+})
+
+const User=mongoose.model("User",userSchema)
+export default User
--- back-end/server.js
+++ back-end/server.js
@@ -2,7 +2,13 @@ import express from 'express'
 import mongoose from 'mongoose';
 const app = express()
 const port=333;
+import router from './routes/authRoute.js'
+mongoose.connect("mongodb://127.0.0.1:27017/social").then(()=>{
+    console.log("connection is done");
+})
 
-// mongoose.connect()
+
+
+app.use('auth',router)
 
 app.listen(port ,()=>console.log('listening on port'))
