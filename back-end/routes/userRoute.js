20971fb309d44cce39dd1534ae94f92d7aa1a9e3
Medo <moaiadefahed2000@gmail.com>
2024-05-27 16:36:53 +0300

    v5

--- back-end/routes/userRoute.js
+++ back-end/routes/userRoute.js
@@ -1,13 +1,13 @@
-import exprees from 'express'
+import express from 'express'
 import {
     getUser,
     getUserFriends,
     addRemoveFriend,
 } from '../controllers/users.js';
 import { checkUserToken } from '../middlewares/Auth.js';
-const router=exprees.Router()
+const router = express.Router()
 router.get('/:id',checkUserToken,getUser)
 router.get('/:id/friends',checkUserToken,getUserFriends)
-router.patch('/:id/:frienId',checkUserToken,addRemoveFriend)
+router.patch('/:id/:friendId',checkUserToken,addRemoveFriend)
 
 export default router
