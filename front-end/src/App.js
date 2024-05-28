c52fd2cf220d31d746c12576b70259dc9e1a4f13
Medo <moaiadefahed2000@gmail.com>
2024-05-28 13:48:04 +0300

    test

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
-
+import {BrowserRouter , Navigate , Routes , Route } from 'react-router-dom'
+import HomPage from './scenes/homePage';
+import LoginPage from './scenes/loginPage';
+import ProfilePage from './scenes/profilePage';
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path = '/' element = {<LoginPage/>}/>
+        <Route path = '/home' element = {<HomPage/>}/>
+        <Route path = '/profile/:userId' element = {<ProfilePage/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
